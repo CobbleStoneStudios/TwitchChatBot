c72c3923af2ce89f224756d0ea273a4509fb6142
Cobbleopolis <cobbleopolis@gmail.com>
2017-04-12 12:15:39 -0400

    Added auto ping response

--- src/main/scala/com/css/cobble/twitchChatBot/TwitchChatChannelHandler.scala
+++ src/main/scala/com/css/cobble/twitchChatBot/TwitchChatChannelHandler.scala
@@ -1,10 +1,14 @@
 package com.css.cobble.twitchChatBot
 
+import com.css.cobble.twitchChatBot.ref.MessageRef
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
 
 class TwitchChatChannelHandler extends ChannelInboundHandlerAdapter {
 
     override def channelRead(ctx: ChannelHandlerContext, msg: Object): Unit = {
+        val msgStr: String = msg.asInstanceOf[String]
+        if(msgStr.contains(MessageRef.PING_MSG))
+            ctx.channel().writeAndFlush(MessageRef.PONG_MSG)
         println(s"Message: $msg")
     }
 
---
+++ src/main/scala/com/css/cobble/twitchChatBot/ref/MessageRef.scala
@@ -0,0 +1,11 @@
+package com.css.cobble.twitchChatBot.ref
+
+object MessageRef {
+
+    final val TWITCH_URL: String = "tmi.twitch.tv"
+
+    final val PING_MSG: String = s"PING: $TWITCH_URL"
+
+    final val PONG_MSG: String = s"PONG: $TWITCH_URL"
+
+}
